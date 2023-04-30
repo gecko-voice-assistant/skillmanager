7415d6685db7e763e893b7ed60a4018e3ffa0fb7
fwehn <finn.wehn@web.de>
2023-04-30 22:10:16 +0200

    implemented first routine from recognition to execution of a skill;

--- src/api/root.js
+++ src/api/root.js
@@ -1,19 +1,12 @@
 const router = require("express").Router();
-const {exec} = require("child_process");
 
 router.get("/", (req, res) => {
     res.send("hello World!");
 });
 
 router.post("/restart", (req, res) => {
-    // todo fix
+    // todo implement restart
     res.json({});
-
-    // exec("npm restart dev", (err) => {
-    //     if (err) console.error(err);
-    //
-    //     res.send("Done");
-    // }, "inherit", "inherit");
 });
 
 module.exports = router;
